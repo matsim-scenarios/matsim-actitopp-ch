92776fccb4c19af67c30a7ae06070b6f070d0290
kainagel <nagel@vsp.tu-berlin.de>
2019-07-11 17:44:54 +0200

    towards locationchoice

--- src/main/java/org/matsim/run/RunMatsim.java
+++ src/main/java/org/matsim/run/RunMatsim.java
@@ -18,16 +18,13 @@
  * *********************************************************************** */
 package org.matsim.run;
 
-import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
-import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.network.Link;
-import org.matsim.api.core.v01.network.NetworkFactory;
-import org.matsim.api.core.v01.network.Node;
-import org.matsim.api.core.v01.population.*;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.contrib.analysis.kai.KaiAnalysisListener;
 import org.matsim.contrib.locationchoice.frozenepsilons.FrozenTastes;
 import org.matsim.contrib.locationchoice.frozenepsilons.FrozenTastesConfigGroup;
@@ -38,20 +35,16 @@ import org.matsim.core.config.groups.StrategyConfigGroup;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
-import org.matsim.core.controler.events.ShutdownEvent;
-import org.matsim.core.controler.listener.ShutdownListener;
 import org.matsim.core.gbl.Gbl;
-import org.matsim.core.population.PopulationUtils;
+import org.matsim.core.gbl.MatsimRandom;
 import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule;
-import org.matsim.core.router.TripRouter;
-import org.matsim.core.router.TripStructureUtils;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.core.utils.geometry.CoordUtils;
-import org.matsim.facilities.*;
+import org.matsim.facilities.ActivityFacilities;
+import org.matsim.facilities.ActivityFacilitiesFactory;
+import org.matsim.facilities.ActivityFacility;
+import org.matsim.facilities.ActivityOption;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.Random;
 
 /**
  * @author bcharlton
@@ -66,7 +59,18 @@ public class RunMatsim {
 		RunType runType = RunType.shortRun ;
 		// yy not sure if it makes sense to keep the short/med/longRun differentiation at this level.  kai, jun'19
 
-		Config config = ConfigUtils.loadConfig( args ) ;
+		Config config ;
+//		if ( args==null || args[0]=="null" ){
+			String configfile = "../../shared-svn/projects/snf-big-data/data/scenario/neuenburg_1pct/config.xml" ;
+			config = ConfigUtils.loadConfig( configfile ) ;
+//		} else {
+//			config = ConfigUtils.loadConfig( args );
+//		}
+
+		config.network().setInputFile( "../transport_supply/switzerland_network.xml.gz" );
+		config.plans().setInputFile( "population_1pct_plans_initial-coords.xml.gz" );
+		config.facilities().setInputFile( "facilities_1pct.xml.gz" );
+
 		switch( runType ) {
 			case shortRun:
 				config.controler().setLastIteration( 2 );
@@ -89,7 +93,27 @@ public class RunMatsim {
 			config.planCalcScore().addActivityParams( params );
 		}
 		{
-			PlanCalcScoreConfigGroup.ActivityParams params = new PlanCalcScoreConfigGroup.ActivityParams( "shop" ) ;
+			PlanCalcScoreConfigGroup.ActivityParams params = new PlanCalcScoreConfigGroup.ActivityParams( "work" ) ;
+			params.setTypicalDuration( 8.*3600. );
+			config.planCalcScore().addActivityParams( params );
+		}
+		{
+			PlanCalcScoreConfigGroup.ActivityParams params = new PlanCalcScoreConfigGroup.ActivityParams( "education" ) ;
+			params.setTypicalDuration( 6.*3600. );
+			config.planCalcScore().addActivityParams( params );
+		}
+		{
+			PlanCalcScoreConfigGroup.ActivityParams params = new PlanCalcScoreConfigGroup.ActivityParams( "shopping" ) ;
+			params.setTypicalDuration( 2.*3600. );
+			config.planCalcScore().addActivityParams( params );
+		}
+		{
+			PlanCalcScoreConfigGroup.ActivityParams params = new PlanCalcScoreConfigGroup.ActivityParams( "leisure" ) ;
+			params.setTypicalDuration( 2.*3600. );
+			config.planCalcScore().addActivityParams( params );
+		}
+		{
+			PlanCalcScoreConfigGroup.ActivityParams params = new PlanCalcScoreConfigGroup.ActivityParams( "other" ) ;
 			params.setTypicalDuration( 2.*3600. );
 			config.planCalcScore().addActivityParams( params );
 		}
@@ -126,24 +150,61 @@ public class RunMatsim {
 		FrozenTastesConfigGroup dccg = ConfigUtils.addOrGetModule( config, FrozenTastesConfigGroup.class );;
 		switch( runType ) {
 			case shortRun:
-				dccg.setEpsilonScaleFactors("10.0" );
+				dccg.setEpsilonScaleFactors("10.0,10.0,10.0" );
 				break;
 			case longRun:
 			case medRun:
-				dccg.setEpsilonScaleFactors("10.0" );
+				dccg.setEpsilonScaleFactors("10.0,10.0,10.0" );
 				break;
 			default:
 				throw new RuntimeException( Gbl.NOT_IMPLEMENTED ) ;
 		}
 		dccg.setAlgorithm( FrozenTastesConfigGroup.Algotype.bestResponse );
-		dccg.setFlexibleTypes( "shop" );
+		dccg.setFlexibleTypes( "leisure,shopping,other" );
 		dccg.setTravelTimeApproximationLevel( FrozenTastesConfigGroup.ApproximationLevel.localRouting );
 		dccg.setRandomSeed( 2 );
-		dccg.setDestinationSamplePercent( 5. );
+		dccg.setDestinationSamplePercent( 0.1 );
 
 		// ### SCENARIO: ###
 
 		Scenario scenario = ScenarioUtils.loadScenario( config ) ;
+
+		for( Person person : scenario.getPopulation().getPersons().values() ){
+			Coord homeCoord = null ;
+			for( PlanElement planElement : person.getSelectedPlan().getPlanElements() ){
+				if ( planElement instanceof Activity ) {
+					if ( ((Activity) planElement).getType().equals( "home" ) ) {
+						homeCoord = ((Activity) planElement).getCoord() ;
+						break ;
+					}
+				}
+			}
+			Gbl.assertNotNull( homeCoord );
+			for( PlanElement planElement : person.getSelectedPlan().getPlanElements() ){
+				if ( planElement instanceof Activity ) {
+					if ( ((Activity) planElement).getCoord()==null ) {
+						((Activity) planElement).setCoord( homeCoord );
+					}
+				}
+			}
+		}
+
+		Random rnd = MatsimRandom.getLocalInstance();;
+		final ActivityFacilities facilities = scenario.getActivityFacilities();
+		ActivityFacilitiesFactory ff = facilities.getFactory();;
+		for( ActivityFacility facility : facilities.getFacilities().values() ){
+			double rrr = rnd.nextDouble();;
+			if ( rrr < 0.33 ) {
+				ActivityOption option = ff.createActivityOption( "shopping" ) ;
+				facility.addActivityOption( option );
+			} else if ( rrr < 0.66 ) {
+				ActivityOption option = ff.createActivityOption( "leisure" ) ;
+				facility.addActivityOption( option );
+			} else {
+				ActivityOption option = ff.createActivityOption( "other" ) ;
+				facility.addActivityOption( option );
+			}
+		}
 
 		// ### CONTROL(L)ER: ###
 
